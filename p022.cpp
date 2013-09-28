849e1a04f1c475293ac5db93b5eecde7939b6ad5
Daniel Dara <ddara@g.clemson.edu>
2013-09-28 00:19:10 -0400

    added solution for problem 22

---
+++ p022.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+// sum up the character values in str
+inline int char_sum(string str)
+{
+	int sum = 0;
+
+	for (int i = 0; i < str.length(); i++)
+	{
+		sum += str[i] - 'A' + 1;
+	}
+
+	return sum;
+}
+
+int main()
+{
+	ifstream input;
+	input.open("p022.in", ios::in);
+	vector<string> names;
+
+	// read in the names
+	while (!input.eof())
+	{
+		string name;
+		std::getline(input, name, ',');
+		names.push_back(name.substr(1, name.length() - 2));
+	}
+
+	sort(names.begin(), names.end());
+
+	int total = 0;
+
+	// compute and sum their values
+	for (int i = 0; i < names.size(); i++)
+	{
+		total += (i + 1) * char_sum(names[i]);
+	}
+
+	cout << total << endl;
+
+	return 0;
+}
