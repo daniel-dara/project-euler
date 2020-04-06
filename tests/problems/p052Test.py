f7b3ba0cc3afdd3c93ab70129fe437d17e55c472
Daniel Dara <ddara@g.clemson.edu>
2020-04-05 23:29:50 -0400

    Fix typo

--- tests/problems/p052Test.py
+++ tests/problems/p052Test.py
@@ -3,7 +3,7 @@ from unittest_data_provider import data_provider
 from p052 import solve_problem_052
 
 
-class Problem051Test(unittest.TestCase):
+class Problem052Test(unittest.TestCase):
     @data_provider(lambda: (
         (2, 125874),
         (6, 142857),
