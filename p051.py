290b36dbcc2a6ebfd8584b31bb07ce493c4f9bd9
Daniel Dara <ddara@g.clemson.edu>
2020-04-05 20:11:46 -0400

    Reorganize tests. Reorder p051 function definitions.

--- p051.py
+++ p051.py
@@ -5,17 +5,7 @@ from utils.primesieve import PrimeSieve
 from utils.string import indices_at, unique_chars, replace_char_at_indices
 
 
-def get_family_ids(number: int) -> str:
-    for char in unique_chars(str(number)):
-        char_indices = indices_at(str(number), char)
-
-        for indices in powerset(char_indices):
-            if indices != ():
-                family_id = replace_char_at_indices(str(number), '*', indices)
-                yield family_id
-
-
-def solve_p051(target_family_count: int) -> int:
+def solve_problem_051(target_family_count: int) -> int:
     # A map of family ids to family members
     families: Dict[str, List[int]] = defaultdict(lambda: [])
 
@@ -25,3 +15,13 @@ def solve_p051(target_family_count: int) -> int:
 
             if len(families[family]) == target_family_count:
                 return min(families[family])
+
+
+def get_family_ids(number: int) -> str:
+    for char in unique_chars(str(number)):
+        char_indices = indices_at(str(number), char)
+
+        for indices in powerset(char_indices):
+            if indices != ():
+                family_id = replace_char_at_indices(str(number), '*', indices)
+                yield family_id
