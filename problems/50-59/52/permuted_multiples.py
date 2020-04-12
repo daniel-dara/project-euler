bd572fce4fb25e119597be67f2d7142babcbd1f5
Daniel Dara <ddara@salesforce.com>
2020-04-12 12:20:19 -0400

    Remove 'what' comments that repeated code.

--- problems/50-59/52/permuted_multiples.py
+++ problems/50-59/52/permuted_multiples.py
@@ -3,12 +3,9 @@ from itertools import count
 
 def solve(max_multiplier: int) -> int:
     """Find the smallest positive integer such that each product up to the given multiplier contains the same digits."""
-    # Iterate to infinity.
     for number in count(1):
-        # Calculate the products that must match.
         products = [multiplier * number for multiplier in range(2, max_multiplier + 1)]
 
-        # Determine if the digits of the products match each other.
         if all(contain_same_digits(number, product) for product in products):
             return number
 
