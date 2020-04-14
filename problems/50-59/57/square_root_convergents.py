b444a5bf516f9769ac9303c3e58fdb9055bee398
Daniel Dara <ddara@g.clemson.edu>
2020-04-14 19:56:36 -0400

    Adjust comments. Make base case more readable.

--- problems/50-59/57/square_root_convergents.py
+++ problems/50-59/57/square_root_convergents.py
@@ -1,25 +1,31 @@
+from itertools import repeat
 from typing import Tuple
 
-expansions = {0: (0, 1)}  # Seed the base expansion.
+expansions = {}
 
 
 def solve() -> int:
     """Find the number of fractions containing a numerator with more digits than the denominator in the first
     one-thousand expansions of the given expression.
     """
-    return sum(len(str(expand(x)[0])) > len(str(expand(x)[1])) for x in range(1000))
+    return sum(len(str(expand(n)[0])) > len(str(expand(n)[1])) for n in range(1000))
 
 
-def expand(x: int) -> Tuple[int, int]:
-    # Pull the expanded fraction for this iteration from cache.
-    expanded_numerator, expanded_denominator = expansions[x]
+def expand(n: int) -> Tuple[int, int]:
+    if n == 0:
+        # Handle base case.
+        numerator = 1
+        denominator = 2
+    else:
+        # Pull the expanded fraction for the previous iteration from cache.
+        previous_numerator, previous_denominator = expansions[n - 1]
 
-    # Calculate "1 / (2 + expanded_fraction)
-    numerator = expanded_denominator
-    denominator = (2 * expanded_denominator) + expanded_numerator
+        # Calculate 1 / (2 + previous fraction)
+        numerator = previous_denominator
+        denominator = (2 * previous_denominator) + previous_numerator
 
-    # Cache the new expanded fraction for the next iteration.
-    expansions[x + 1] = numerator, denominator
+    # Cache the expanded fraction for this iteration
+    expansions[n] = numerator, denominator
 
-    # Calculate "1 + new_fraction"
+    # Calculate 1 + fraction
     return numerator + denominator, denominator
